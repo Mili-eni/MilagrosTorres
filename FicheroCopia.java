e127d163a6b12d9506066dc3bc990a6435edc87a
Milagros <mtorresa09@educarex.es>
2022-03-18 12:39:07 +0100

    Todo

---
+++ FicheroCopia.java
@@ -0,0 +1,68 @@
+package array;
+
+import java.util.Scanner;
+
+public class GesNotas {
+
+	static Scanner teclado= new Scanner(System.in);		
+	
+
+	    public static void main (String []args){
+	    	
+	    
+            double []notas = new double [10];	
+           inicializarVector(notas); 
+   		 
+	        int opc;
+	      
+       	do {
+		    System.out.print("Elige una opción:");
+	        opc=teclado.nextInt(); 
+	        System.out.print("Opción 1: mostrar toda las notas introducidas: ");
+	        System.out.print("Opción 2: ordenar de menor a mayor las notas: ");
+	        System.out.print("Opción 3: buscar una nota en la lista: ");
+	        System.out.print("Opción 4: mostrar la nota media: ");
+	        System.out.print("Opción 5: mostrar el número de aprobados: ");
+	        System.out.print("Opción 6: mostrar la nota mas alta: ");
+    // comentario
+	        switch (opc) {
+	        case 1: 
+	     	break;
+	     	
+	        case 2:
+	     	break;
+	     	
+	        case 3:
+		    break;
+		    
+	        case 4:
+	        break;
+	        	
+	        case 5:
+	        break;
+	        	
+	        case 6:
+	        break;
+	        	
+		
+	        }
+
+      }  while (opc != 0) ;
+	     System.out.print("Opción no válida");
+      
+       	
+       	}
+	    	    
+	
+	
+	}
+       	
+
+	
+       	
+	    
+		
+       	
+	
+
+	
